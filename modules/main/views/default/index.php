c1eaf2d880a6520314880744292159924f4f4333
Kiberhach_ <kiberhach@bk.ru>
2016-11-01 11:05:08 +0300

    test commit from notebook

--- modules/main/views/default/index.php
+++ modules/main/views/default/index.php
@@ -14,7 +14,8 @@ $this->title = 'My Yii Application';
             }
         ?>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">You have successfully created your Yii-powered application. It was added from notebook for testing sending
+         commit on github</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
