91e73cccf2720a3d35531c83540c04c3c98391fe
Cyberha4 <proffi99@mail.ru>
2017-04-28 19:02:58 +0300

    added some forms for modules User

---
+++ modules/user/mail/emailConfirm.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cyberha4
+ * Date: 28.04.2017
+ * Time: 18:11
+ */
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user app\modules\user\models\User */
+
+$confirmLink = Yii::$app->urlManager->createAbsoluteUrl(['user/default/email-confirm', 'token' => $user->email_confirm_token]);
+?>
+
+Здравствуйте, <?= Html::encode($user->username) ?>!
+
+Для подтверждения адреса пройдите по ссылке:
+
+<?= Html::a(Html::encode($confirmLink), $confirmLink) ?>
+
+Если Вы не регистрировались у на нашем сайте, то просто удалите это письмо.
